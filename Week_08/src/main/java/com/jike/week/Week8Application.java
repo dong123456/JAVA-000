b5635581fb317a7b09ece9a8e5505c64ca92c823
zdd <zhangdongdong@qudian.com>
2020-12-10 11:36:42 +0800

    调整版本号

--- Week_08/src/main/java/com/jike/week/Week8Application.java
+++ Week_08/src/main/java/com/jike/week/Week8Application.java
@@ -2,13 +2,16 @@ package com.jike.week;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@MapperScan(value= "")
 public class Week8Application {
 
 	public static void main(String[] args) {
