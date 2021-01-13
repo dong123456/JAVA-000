9492b2b1d54858f8ae1aaba8982211df281c5feb
zdd <zhangdongdong@qudian.com>
2021-01-13 18:33:05 +0800

    测试消息

--- Week_13/activemq/src/main/java/io/kimmking/javacourse/mq/activemq/ActiveMQServer.java
+++ Week_13/activemq/src/main/java/io/kimmking/javacourse/mq/activemq/ActiveMQServer.java
@@ -8,9 +8,10 @@ import java.util.function.Consumer;
 
 public class ActiveMQServer {
 
-    public static void main(String[] args) {
-        // 尝试用java代码启动一个ActiveMQ broker server
-        // 然后用前面的测试demo代码，连接这个嵌入式的server
+    public static void main(String[] args) throws JMSException {
+        queueProducer();
+
+        queueConsumer();
 
     }
 
